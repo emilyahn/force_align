41a333147ff218c0d4f834c35c2cf4271a5bd7a3
Emily Ahn <eahn@wellesley.edu>
2023-08-07 15:38:11 -0700

    updated eval script

--- src/eval_align.py
+++ src/eval_align.py
@@ -58,25 +58,37 @@ if __name__ == '__main__':
 
 		pred_dict[file_id_pred] = utt_list
 
-	# evaluate
-	if args.command == 'boundary_diff':
-		diffs = []
-		for file_id, gold_list in gold_dict.items():
-			pred_list = pred_dict[file_id]
-			# if len(gold_list) != len(pred_list):
-			# 	print(file_id)  # all good!
-			for i_tup, gold_tup in enumerate(gold_list):
-				pred_tup = pred_list[i_tup]
-				# absolute diff of each phone's ONSET only
-				# if want OFFSET, use tup[2]
-				diff = abs(gold_tup[1] - pred_tup[1])
-				diffs.append(diff)
+	diffs = []
+	for file_id, gold_list in gold_dict.items():
+		pred_list = pred_dict[file_id]
+		# if len(gold_list) != len(pred_list):
+		# 	print(file_id)  # all good!
+		for i_tup, gold_tup in enumerate(gold_list):
+			pred_tup = pred_list[i_tup]
+			# absolute diff of each phone's ONSET only
+			# if want OFFSET, use tup[2]
+			diff = abs(gold_tup[1] - pred_tup[1])
+			diffs.append(diff)
 
-		print(f'MEAN: {statistics.mean(diffs)}')
+	# evaluate
+	if not args.command or args.command == 'boundary_diff':
+
+		print(f'MEAN: {statistics.mean(diffs):0.2f}')
 		print(f'MEDIAN: {statistics.median(diffs)}')
 		print(f'MIN: {min(diffs)}')
 		print(f'MAX: {max(diffs)}')
 
-	elif args.command == 'accuracy_window':
-		window_msec = args.window_msec
+	if not args.command or args.command == 'accuracy_window':
+		if not args.window_msec:
+			windows = [10, 20]
 
+		else:
+			windows = [args.window_msec]
+
+		for window_msec in windows:
+			within_window = 0
+			for diff_item in diffs:
+				if diff_item < window_msec:
+					within_window += 1
+
+			print(f'WINDOW ({window_msec}ms): {float(within_window/len(diffs)):0.2f}')
